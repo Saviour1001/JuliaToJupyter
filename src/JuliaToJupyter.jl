d453e38c2e58a6e1566119241462b8308aa4785e
Saviour1001 <harshghodkar@gmail.com>
2021-04-21 20:24:51 +0530

    Got the cells working Yay!

--- src/JuliaToJupyter.jl
+++ src/JuliaToJupyter.jl
@@ -3,12 +3,11 @@ module JuliaToJupyter
 # Write your package code here.
 f = open("test\\Test_1.jl","r")
 line = 0
+file = open("writing files\\Test_1.ipynb","w")
+write(file,"{ \"cells\":[ ")
 while !eof(f)
     line+=1
-    file = open("writing files\\Test_1.ipynb","w")
-
     s = readline(f)
-    write(file,"{")
     if (s[1]=='#')
         md = open("templates\\markdown_cell.json","r")
         println("Its a comment!!")
@@ -17,6 +16,7 @@ while !eof(f)
             mdr = readline(md)
             write(file,"$mdr")
         end
+        write(file,",")
         close(md)
     else
         cc = open("templates\\code_cell.json","r")
@@ -26,14 +26,17 @@ while !eof(f)
             ccr = readline(cc)
             write(file,"$ccr")
         end
+        write(file,",")
         close(cc)
     end
-    template = open("templates\\metadata.json","r")
-    while !eof(template)
-        metaR = readline(template)
-        write(file,"$metaR")
-    end
-    write(file,"}")
-    close(file)
+
 end
+write(file,"],")
+template = open("templates\\metadata.json","r")
+while !eof(template)
+    metaR = readline(template)
+    write(file,"$metaR")
+end
+write(file,"}")
+close(file)
 end
