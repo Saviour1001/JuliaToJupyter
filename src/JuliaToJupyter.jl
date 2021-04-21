0d5ed1caa0d60e126a2d7dcef1ae26ff8353c8df
Saviour1001 <harshghodkar@gmail.com>
2021-04-22 01:08:10 +0530

    update

--- src/JuliaToJupyter.jl
+++ src/JuliaToJupyter.jl
@@ -4,30 +4,30 @@ module JuliaToJupyter
 using JSON
 f = open("test\\Test_1.jl","r")
 file = open("writing files\\Test_1.ipynb","w")
+squareBrackets="[]"
+curlyBrackets="{}"
 while !eof(f)
     s = readline(f)
     if (s[1]=='#')
         println("Its a comment")
         line = Dict(
             "cell_type"=>"markdown",
-            "metadata"=>{},
+            "metadata"=>curlyBrackets,
             "source"=>"[$s]"
             )
-        json_object = json(line)
+        json_object = json(line,4)
         write(file,json_object)
-        write(file,"\n")
     else
         println("Its a code line")
         line = Dict(
             "cell_type"=>"code",
             "execution_count"=>"null",
-            "metadata"=>"{}",
-            "outputs"=>"[]",
+            "metadata"=>curlyBrackets,
+            "outputs"=>squareBrackets,
             "source"=>"[$s]"
         )
-        json_object = json(line)
+        json_object = json(line,2)
         write(file,json_object)
-        write(file,"\n")
     end
 end
 close(f)
