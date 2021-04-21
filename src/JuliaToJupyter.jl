f146fa117c96f7993ce5e22ccd9c75b5d0a21adb
Saviour1001 <harshghodkar@gmail.com>
2021-04-21 19:19:11 +0530

    Tried something

--- src/JuliaToJupyter.jl
+++ src/JuliaToJupyter.jl
@@ -2,20 +2,38 @@ module JuliaToJupyter
 
 # Write your package code here.
 f = open("test\\Test_1.jl","r")
-file = open("writing files\\Test_1.ipynb","w")
+template = open("templates\\metadata.json","r")
 line = 0
 while !eof(f)
-    s = readline(f)
     line+=1
+    file = open("writing files\\Test_1.ipynb","w")
 
+    s = readline(f)
+    write(file,"{")
     if (s[1]=='#')
+        md = open("templates\\markdown_cell.json","r")
         println("Its a comment!!")
-        write(file,"$s")
+        println("Line Number: $line")
+        while !eof(md)
+            mdr = readline(md)
+            write(file,"$mdr")
+        end
+        close(md)
     else
+        cc = open("templates\\markdown_cell.json","r")
         println("Its a code line")
+        println("Line Number: $line")
+        while !eof(cc)
+            ccr = readline(cc)
+            write(file,"$ccr")
+        end
+        close(cc)
     end
-
-
+    while !eof(template)
+        metaR = readline(template)
+        write(file,"$metaR")
+    end
+    write(file,"}")
+    close(file)
 end
-
 end
