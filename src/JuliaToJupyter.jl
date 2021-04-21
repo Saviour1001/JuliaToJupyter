37b2fb495d536f05c106e42630275e0aa7ac6bec
Saviour1001 <harshghodkar@gmail.com>
2021-04-21 18:46:16 +0530

    Writing into python notebooks

--- src/JuliaToJupyter.jl
+++ src/JuliaToJupyter.jl
@@ -2,6 +2,7 @@ module JuliaToJupyter
 
 # Write your package code here.
 f = open("test\\Test_1.jl","r")
+file = open("writing files\\Test_1.ipynb","w")
 line = 0
 while !eof(f)
     s = readline(f)
@@ -9,6 +10,7 @@ while !eof(f)
 
     if (s[1]=='#')
         println("Its a comment!!")
+        write(file,"$s")
     else
         println("Its a code line")
     end
