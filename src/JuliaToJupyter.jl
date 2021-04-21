fef1787f29b3e1a899e8cf9663fb3e9f0f081815
Saviour1001 <harshghodkar@gmail.com>
2021-04-21 14:58:17 +0530

    Added the parser

--- src/JuliaToJupyter.jl
+++ src/JuliaToJupyter.jl
@@ -6,7 +6,18 @@ line = 0
 while !eof(f)
     s = readline(f)
     line+=1
-    println("$line . $s")
+
+    if (s[1]=='#')
+        println("Its a comment!!")
+    else
+        println("Its a code line")
+    end
+
+
 end
+
+
+
+
 
 end
