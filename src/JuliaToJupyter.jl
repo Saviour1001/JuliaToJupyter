db4a2b5455d391816d28d3d9ceafdf8e03c83b48
Saviour1001 <harshghodkar@gmail.com>
2021-05-09 12:55:40 +0530

    added the metadata template to the notebook

--- src/JuliaToJupyter.jl
+++ src/JuliaToJupyter.jl
@@ -4,15 +4,16 @@ module JuliaToJupyter
 using JSON
 f = open("test\\Test_1.jl","r")
 file = open("writing files\\Test_1.ipynb","w")
-squareBrackets="[]"
-curlyBrackets="{}"
+start = "{ \n \"cells\": [\n"
+finish = "],"
+write(file,start)
 while !eof(f)
     s = readline(f)
     if (s[1]=='#')
         println("Its a comment")
         line = Dict(
             "cell_type"=>"markdown",
-            "metadata"=>curlyBrackets,
+            "metadata"=>"{}",
             "source"=>"[$s]"
             )
         json_object = json(line,4)
@@ -22,14 +23,21 @@ while !eof(f)
         line = Dict(
             "cell_type"=>"code",
             "execution_count"=>"null",
-            "metadata"=>curlyBrackets,
-            "outputs"=>squareBrackets,
+            "metadata"=>"{}",
+            "outputs"=>"[]",
             "source"=>"[$s]"
         )
         json_object = json(line,2)
         write(file,json_object)
     end
 end
+write(file,finish)
+metadata_template = open("templates\\metadata.json")
+while !eof(metadata_template)
+    s = readline(metadata_template)
+    write(file,s)
+end
+write(file,"}")
 close(f)
 close(file)
 end
